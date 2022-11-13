34489a72596b0eb67be4312f705c5ade9c3cd055
PiotrekSy <piotrsyjud@gmail.com>
2022-11-13 17:57:07 +0100

    220211131757

--- GuessingGame/components/ui/Title.ios.js
+++ GuessingGame/components/ui/Title.ios.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, Platform } from 'react-native';
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -13,8 +13,12 @@ const styles = StyleSheet.create({
     // fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
+    // borderWidth: Platform.OS==='android'? 1 : 4,
+    // borderWidth: Platform.select({ android: 0, ios: 1 }),
     borderWidth: 2,
     borderColor: 'white',
     padding: 12,
+    maxWidth: '80%',
+    minWidth: 300,
   },
 });
---
+++ GuessingGame/constants/colors.android.js
@@ -0,0 +1,9 @@
+const Colors = {
+  primary500: '#72063c',
+  primary600: '#640233',
+  primary700: '#420423',
+  primary800: '#3b021f',
+  accent500: '#fac411'
+};
+
+export default Colors;
